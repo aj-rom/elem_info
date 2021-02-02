98c1c229d164097e6991cb8fd3066849c1abb757
A.J. Romaniello <ajiellodev@gmail.com>
2021-02-02 19:08:52 +0000

    Updated

--- Gemfile
+++ Gemfile
@@ -1,4 +1,6 @@
 source "https://rubygems.org"
+gem "open-uri"
+gem "nokogiri"
 
 # Specify your gem's dependencies in elem_info.gemspec
 gemspec
--- lib/elem_info.rb
+++ lib/elem_info.rb
@@ -1,4 +1,7 @@
-require "elem_info/version"
+require_relative "elem_info/version"
+require_relative "elem_info/cli"
+require_relative "elem_info/scraper"
+require_relative "elem_info/element"
 
 module ElemInfo
   class Error < StandardError; end
