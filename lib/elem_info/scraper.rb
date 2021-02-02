35bd57aaf4d48d848c50d9542963dd422adc7886
CoachLuck <ajiellodev@gmail.com>
2021-02-02 14:13:48 -0700

    Link scraping to elements

--- lib/elem_info/scraper.rb
+++ lib/elem_info/scraper.rb
@@ -1,3 +1,21 @@
-class ElemInfo::Scraper
+# frozen_string_literal: true
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
 
+module ElemInfo
+
+  # Used for scraping element properties from ptable.com
+  class Scraper
+
+    def initialize
+      @doc = Nokogiri::HTML(URI.open('https://ptable.com/#'))
+      load_elements
+    end
+
+    def load_elements
+      Element.all_from_ptable_doc(@doc.css("ol li"))
+    end
+
+  end
 end
