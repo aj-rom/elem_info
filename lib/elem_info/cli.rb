6541648f43b4c3ce7afc1697f1cafec20fe133b6
CoachLuck <ajiellodev@gmail.com>
2021-02-02 14:14:06 -0700

    add class methods for program flow

--- lib/elem_info/cli.rb
+++ lib/elem_info/cli.rb
@@ -1,3 +1,40 @@
-class ElemInfo::CLI
+# frozen_string_literal: true
+
+require 'colorize'
+
+module ElemInfo
+
+  # Main CLI class for ElemInfo
+  class CLI
+
+    def initialize
+      print_welcome
+      load_all_elements
+      display_all_elements
+    end
+
+    def load_all_elements
+      Scraper.new.load_elements
+    end
+
+    def display_all_elements
+      Element.all.each_with_index { |e, i| puts `#{i + 1}`.white + e.name.blue }
+    end
+
+    def execute_command_from_input(input)
+      # execute
+    end
+
+    def print_welcome
+      puts 'Welcome to'.yellow + ' ElemInfo'.blue + ' ( Element Information )'.yellow
+    end
+
+    def prompt(question)
+      puts ''
+      puts question.yellow
+      gets.chomp
+    end
+
+  end
 
 end
