59392c52bf3d625c6481d9f59fcd264c24caa731
CoachLuck <ajiellodev@gmail.com>
2021-02-03 12:55:10 -0700

    Add Properties class to environment

--- lib/elem_info.rb
+++ lib/elem_info.rb
@@ -2,6 +2,7 @@ require_relative "elem_info/version"
 require_relative "elem_info/cli"
 require_relative "elem_info/scraper"
 require_relative "elem_info/element"
+require_relative 'elem_info/properties'
 
 module ElemInfo
   class Error < StandardError; end
