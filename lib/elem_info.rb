3e70b32142955f602dd5dbe01245bc0351f7ac81
CoachLuck <ajiellodev@gmail.com>
2021-02-03 15:57:57 -0700

    Add compounds

--- lib/elem_info.rb
+++ lib/elem_info.rb
@@ -3,6 +3,7 @@ require_relative "elem_info/cli"
 require_relative "elem_info/scraper"
 require_relative "elem_info/element"
 require_relative 'elem_info/properties'
+require_relative 'elem_info/compound'
 
 module ElemInfo
   class Error < StandardError; end
