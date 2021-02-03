d36cbef425a50d51056592b7a5948b0456f6bf72
CoachLuck <ajiellodev@gmail.com>
2021-02-03 12:19:06 -0700

    Add way more data for the element class to be able to store from the initial scrape

--- lib/elem_info/element.rb
+++ lib/elem_info/element.rb
@@ -4,15 +4,29 @@ module ElemInfo
 
   # Holds basic element information and has some class methods to find other useful information
   class Element
-    attr_accessor :name, :atomic_number, :atomic_weight, :symbol
+    attr_reader :name, :atomic_number, :atomic_weight, :symbol, :etymolygy, :group, :period, :melting_point,
+    :boiling_point, :density, :heat_cap, :electro_negativity, :abundance
 
     @@all = []
 
-    def initialize(name, atomic_number, atomic_weight, symbol)
+    def initialize(name, symbol, etymolygy, atomic_number, atomic_weight, group, period,
+                   density, melting_point, boiling_point, heat_cap, electro_negativity, abundance)
       @name = name
+      @symbol = symbol
+      @etymolygy = etymolygy
       @atomic_number = atomic_number
       @atomic_weight = atomic_weight
-      @symbol = symbol
+      @group = group
+      @period = period
+      @density = density
+      @melting_point = melting_point
+      @boiling_point = boiling_point
+      @heat_cap = heat_cap
+      @electro_negativity = electro_negativity
+      @abundance = abundance
+    end
+
+    def save
       @@all << self
     end
 
@@ -26,16 +40,39 @@ module ElemInfo
 
     def self.all_from_table_doc(doc)
       doc.each do |e|
-        from_table_element(e) unless e.attributes.length.zero?
+        from_table_element(e) unless e.children.length <= 1
       end
     end
 
     def self.from_table_element(e)
-      an = e.css("b").text
-      aw = e.css("data").text
-      n = e.css("em").text
-      s = e.css("abbr").text
-      new(n, an, aw, s)
+      elem = e.css("td")
+      puts "#{elem}"
+      atomic_n = elem[0].text.strip.to_i
+      symbol = elem[1].text.strip
+      name = elem[2].text.strip
+      puts "#{name}"
+      etym = elem[3].text.strip
+      group = elem[4].text.strip.to_i
+      period = elem[5].text.strip.to_i
+      atomic_w = fix_digit(elem[6])
+      density = fix_digit(elem[7])
+      melting = fix_digit(elem[8])
+      boiling = fix_digit(elem[9])
+      heat_cap = fix_digit(elem[10])
+      elec_neg = fix_digit(elem[11])
+      abundance = fix_digit(elem[12])
+
+      new(name, symbol, etym, atomic_n, atomic_w, group, period, density, melting,
+          boiling, heat_cap, elec_neg, abundance).save
+    end
+
+    def self.fix_digit(str)
+      str = str.text.strip
+      if str == "–"
+        return "N/A"
+      end
+
+      str.tr("^0-9.", "").to_i
     end
 
     def self.get_from_name(name)
