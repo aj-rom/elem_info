ee8d7b8da6745dfccfe96320114a664e3d40434b
CoachLuck <ajiellodev@gmail.com>
2021-02-04 10:53:29 -0700

    Add sorting methods (name, weight, group, period)

--- lib/elem_info/element.rb
+++ lib/elem_info/element.rb
@@ -31,10 +31,6 @@ module ElemInfo
       puts "#{"Summary".blue}: #{description}"
     end
 
-    def protons
-      atomic_number
-    end
-
     def self.all
       @@all
     end
@@ -71,8 +67,59 @@ module ElemInfo
       if e.to_i > 0
         get_from_atomic_number(e)
       else
-        e.length < 3 ? get_from_symbol(e) : get_from_name(name)
+        e.length < 3 ? get_from_symbol(e) : get_from_name(e)
       end
+    end
+
+    def self.display_all
+      puts "Here are all of the currently known elements in order of atomic number:".yellow
+      display_elements(all)
+    end
+
+    def self.display_by_group(group)
+      puts "#{"Group".blue}: #{group}"
+      display_elements(get_by_group(group))
+    end
+
+    def self.display_by_period(period)
+      puts "#{"Period".blue}: #{period}"
+      display_elements(get_by_period(period))
+    end
+
+    def self.display_by_weight
+      puts "Here are all of the elements sorted by Atomic Weight:".yellow
+      puts sort_by_weight.map { |e| "#{e.name.red} (#{e.properties.atomic_weight.round(4).to_s.blue})"}.join(", ")
+    end
+
+    def self.display_by_name
+      puts "Here are all of the elements sorted by name:".yellow
+      puts sort_by_name.map { |e| "#{e.name.red} (#{e.symbol.blue})"}.join(", ")
+    end
+
+    def self.display_elements(elems)
+      map = elems.map do |e|
+        num = e.atomic_number
+        name = e.name.red
+        "#{num.to_s.white}. #{name} (#{e.symbol.blue})#{(num % 7).zero? ? "\n" : ', '}"
+      end
+
+      puts map.uniq.join
+    end
+
+    def self.get_by_group(group)
+      all.select { |e| e.properties.group == group }
+    end
+
+    def self.get_by_period(period)
+      all.select { |e| e.properties.period == period }
+    end
+
+    def self.sort_by_name
+      all.sort { |a, b| a.name <=> b.name }
+    end
+
+    def self.sort_by_weight
+      all.sort { |a, b| a.properties.atomic_weight <=> b.properties.atomic_weight }
     end
 
   end
