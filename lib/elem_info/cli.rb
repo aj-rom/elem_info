32a5c3d53945b6fb688d205dd4fa7136ab0c65bd
CoachLuck <ajiellodev@gmail.com>
2021-02-04 10:53:39 -0700

    Add main CLI loop

--- lib/elem_info/cli.rb
+++ lib/elem_info/cli.rb
@@ -10,33 +10,79 @@ module ElemInfo
     def initialize
       print_welcome
       load_all_elements
-      display_all_elements
-      prompt_choose_element
+      start
     end
 
-    # move to elements class?
     def load_all_elements
       Scraper.load_elements
     end
 
-    # move to elements class
     def display_all_elements
-      puts 'Here are all of the currently known elements sorted by atomic number:'.yellow
-      map = Element.all.map do |e|
-        num = e.atomic_number
-        name = e.name.red
-        "#{num.to_s.white}. #{name} (#{e.symbol.blue})#{(num % 7).zero? ? "\n" : ', '}"
-      end
-      puts map.uniq.join
+      Element.display_all
+    end
+
+    def start
+      x = prompt("What would you like to do? (Sort, Inspect, Compound, Exit)").downcase
+
+      execute_command_from_input(x)
     end
 
     def execute_command_from_input(input)
-      # execute
+      i = input[0]
+      if i == "e"
+        puts "Goodbye!".blue
+      else
+        if i == "s"
+          prompt_to_sort
+        elsif i == "i"
+          display_all_elements
+          prompt_choose_element
+        elsif i == "c"
+          display_all_elements
+          prompt_to_create_compound
+        else
+          puts "Invalid option!".red
+        end
+        start
+      end
     end
 
     def print_welcome
       puts 'Welcome to'.yellow + ' ElemInfo'.blue + ' ( Element Information )'.yellow
       puts "\n"
+    end
+
+    def prompt_to_sort
+      x = prompt("How would you like to sort by? (Name, Group, Period, Weight)").downcase
+
+      display_sort(x)
+    end
+
+    def display_sort(type)
+      case(type[0])
+        when "n"
+          Element.display_by_name
+        when "g"
+          prompt_sort_by_group
+        when "p"
+          prompt_sort_by_period
+        when "w"
+          Element.display_by_weight
+        else
+          prompt_to_sort
+      end
+    end
+
+    def prompt_sort_by_group
+      x = prompt("Please enter the number of the group to sort by:").to_i
+
+      x < 1 || x > 18 ? prompt_sort_by_group : Element.display_by_group(x)
+    end
+
+    def prompt_sort_by_period
+      x = prompt("Please enter the number of the group to sort by:").to_i
+
+      x < 1 || x > 7 ? prompt_sort_by_group : Element.display_by_period(x)
     end
 
     def prompt_to_create_compound
